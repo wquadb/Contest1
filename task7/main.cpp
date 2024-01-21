0ffddaf7dbb6069c0569fd7434c1624b57b3dd01
wquadb <esavdeev@edu.hse.ru>
2024-01-21 16:26:48 +0300

    tasks7/8 are done

--- task7/main.cpp
+++ task7/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <iomanip>
 
 int main() {
+    std::cout << "  \t";
+    for (int i = 0; i < 16; i++) {
+        std::cout << std::hex << std::uppercase << i << "\t";
+    }
 
-
-
+    for (int i = 0; i < 8; i++) {
+        std::cout << '\n';
+        if (i < 3) {
+            std::cout << i << "\t";
+        } else {
+            std::cout << i << "\t";
+            for (int j = 0; j < 16; j++) {
+                int ascii_code = i * 16 + j;
+                std::cout << (char)ascii_code << "\t";
+            }
+        }
+    }
     return 0;
 }
--- task8/main.cpp
+++ task8/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
+#include <map>
 
 int main() {
-
-
-
+    int day, month, year;
+    std::cin >> day >> month >> year;
+    std::map<int, int> m = {
+        {1, 31},{2, 28},{3, 31},{4, 30}, 
+        {5, 31},{6, 30},{7, 31},{8, 31},
+        {9, 30},{10, 31},{11, 30},{12, 31}
+    };
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        m[2] = 29;
+    }
+    if (day + 2 > m[month]) {
+        day = day + 2 - m[month];
+        if (month == 12) {
+            month = 1;
+            year += 1;
+        } else {
+            month += 1;
+        }
+    } else {
+        day += 2;
+    }
+    std::cout << day << " " << month << " " << year;
     return 0;
-}
+} 
