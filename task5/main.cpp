921e7c189e91bf63e2dfc54ed4ea7a791c91b37f
wquadb <esavdeev@edu.hse.ru>
2024-01-21 15:51:02 +0300

    tasks5-6 have been done

--- task5/main.cpp
+++ task5/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+int r(int n, int m, int k) {
+    if (n < m) {
+        return 0;
+    } else if (n == m) {
+        return m / k;
+    } else {
+        return (n / m) * (m / k) + r((n / m) * (m % k) + (n % m), m, k);
+    }
+}
 
 int main() {
-
-
-
-    return 0;
+    int n, m, k;
+    std::cin >> n >> m >> k;
+    if (m < k || n < m) {
+        std::cout << 0 << std::endl;
+        return 0;
+    } else if (m == k) {
+        std::cout << n / m << std::endl;
+        return 0;
+    } else {
+        std::cout << r(n, m, k) << std::endl;
+        return 0;
+    }
 }
--- task6/main.cpp
+++ task6/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
-
 int main() {
-
-
-
+    int n, m;
+    std::cin >> n >> m;
+    std::cout << "\t";
+    for (int j = 1; j <= n; ++j) {
+        std::cout << j << "\t";
+    }
+    for (int i = 1; i <= n; ++i) {
+        std::cout << "\n" << i << "\t"; 
+        for (int j = 1; j <= n; ++j) {
+            std::cout << (i * j) % m << "\t";
+        }
+    }
     return 0;
 }
